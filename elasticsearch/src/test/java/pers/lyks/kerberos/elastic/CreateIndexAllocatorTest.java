d7d5c4bd8a57e590933caace7d470ff06889520d
lawyerance <lawyerance@gmail.com>
2019-12-05 21:01:14 +0800

    [MOD] logback configuration use groovy

--- elasticsearch/src/test/java/pers/lyks/kerberos/elastic/CreateIndexAllocatorTest.java
+++ elasticsearch/src/test/java/pers/lyks/kerberos/elastic/CreateIndexAllocatorTest.java
@@ -5,6 +5,9 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 import net.minidev.json.JSONArray;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 
 import java.util.Map;
@@ -17,44 +20,13 @@ public class CreateIndexAllocatorTest {
 
     @Test
     public void compatible() {
-        String json = "{\n" +
-                "\t\"settings\": {},\n" +
-                "\t\"mappings\": {\n" +
-                "\t\t\"doc\": {\n" +
-                "\t\t\t\"dynamic\": false,\n" +
-                "\t\t\t\"properties\": {\n" +
-                "\t\t\t\t\"name\": {\n" +
-                "\t\t\t\t\t\"type\": \"keyword\"\n" +
-                "\t\t\t\t},\n" +
-                "\t\t\t\t\"title\": {\n" +
-                "\t\t\t\t\t\"index\": true,\n" +
-                "\t\t\t\t\t\"type\": \"text\"\n" +
-                "\t\t\t\t},\n" +
-                "\t\t\t\t\"age\": {\n" +
-                "\t\t\t\t\t\"type\": \"integer\"\n" +
-                "\t\t\t\t}\n" +
-                "\t\t\t}\n" +
-                "\t\t}\n" +
-                "\t},\n" +
-                "\t\"aliases\": {}\n" +
-                "}";
-        Configuration.ConfigurationBuilder builder = Configuration.builder();
-        builder.options(Option.ALWAYS_RETURN_LIST);
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
 
-        DocumentContext parse = JsonPath.parse(json, builder.build());
-        JSONArray array = parse.read("$.mappings.*");
-        parse.delete("$.mappings.*");
-        System.out.println(parse.jsonString());
-        for (Object item : array) {
-            Map<String, Object> map = (Map<String, Object>) item;
-            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                parse = parse.put("$.mappings", entry.getKey(), entry.getValue());
-                System.out.println(parse.jsonString());
+        boolQueryBuilder.should(QueryBuilders.matchQuery("name", "index-001"));
 
-            }
-        }
-
-//        parse.add("$.mappings", array);
-        System.out.println(parse.jsonString());
+        boolQueryBuilder.should(QueryBuilders.matchQuery("indexCode", "430004403"));
+        builder.query(boolQueryBuilder);
+        System.out.println(builder);
     }
 }
