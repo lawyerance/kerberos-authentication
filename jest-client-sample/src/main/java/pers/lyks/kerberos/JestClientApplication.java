0f593842130df31819fef14408029c797b087893
lawyerance <lawyerance@gmail.com>
2019-12-07 09:43:08 +0800

    [ADD] jest client sample

--- jest-client-sample/src/main/java/pers/lyks/kerberos/JestClientApplication.java
+++ jest-client-sample/src/main/java/pers/lyks/kerberos/JestClientApplication.java
@@ -1,12 +1,14 @@
 package pers.lyks.kerberos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 /**
  * @author lawyerance
  * @version 1.0 2019-11-22
  */
+@SpringBootApplication
 public class JestClientApplication {
 
     public static void main(String[] args) {
